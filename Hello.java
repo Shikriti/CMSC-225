20af301f15ca55c3ae30e84aff7ced067d8e6b5c
Shikriti <89548008+Shikriti@users.noreply.github.com>
2021-08-25 16:26:08 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+/*Hello.java
+Shikriti Ghosh
+8/25/2021
+CMSC-225-004*/
+public class Hello {
+	public static void main(String [ ] args) {
+
+		System.out.println("Shikriti Ghosh") ;
+		System.out.println("Hello.java") ;
+		System.out.println("Intro to Programming") ;
+		System.out.println("CMSC-225-004") ;
+		System.out.println() ;
+
+	}
+}
